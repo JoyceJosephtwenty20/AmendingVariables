46cff39806a95cd5e830d33db373a545d21f115f
joycejosephtwenty20 <joyce.j106s13@gmail.com>
2020-06-22 15:15:36 -0400

    FirstCommit

---
+++ README.md
@@ -0,0 +1,20 @@
+"# AmendingVariables"
+
+
+Variables
+
+	Start Year = 1
+	End Year = 5 
+	Current Income = 62000
+	Yearly Increase = 10%
+
+Process: 
+	Use the while operation to loop the 10% yearly increase 5 times to get the income after 5 years. 
+	while year <= 5
+		Then do the below steps until years equals to 5.
+
+End
+
+
+
+
---
+++ src/AmendingVariables.java
@@ -0,0 +1,15 @@
+public class AmendingVariables {
+    public static void main(String[] args) {
+        int robertCurrentIncome = 62000;
+        float yearlyIncrease = 1.1f;
+        System.out.println("Robert's starting income:" + robertCurrentIncome);
+        int years = 1;
+        while (years <= 5) {
+            years ++;
+            robertCurrentIncome = (int) (robertCurrentIncome * yearlyIncrease);
+        }
+
+        System.out.println("Robert income after 5 year if increased by 10% each year: " + robertCurrentIncome);
+    }
+}
+
